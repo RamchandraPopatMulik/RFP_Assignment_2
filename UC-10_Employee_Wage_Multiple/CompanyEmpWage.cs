3d0601f6cf7ab688c23c0161be667e5ac17ff452
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-16 11:20:33 -0700

    added file

---
+++ UC-10_Employee_Wage_Multiple/CompanyEmpWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_10_Employee_Wage_Multiple
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+
+        }
+        public string toString()
+        {
+            return "Total Emp Wage For company :" + this.company + "is : " + this.totalEmpWage;
+        }
+    }
+}
---
+++ UC-10_Employee_Wage_Multiple/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UC_10_Employee_Wage_Multiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addcompanyWage("TCS",20,2,10);
+            empWageBuilder.addcompanyWage("TechMahindra",10,4,20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
